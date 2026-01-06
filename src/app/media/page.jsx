4c943b103a26b0c94a4a1519be17532b9ac28cf2
Akshansh Verma <akshanshverma53@gmail.com>
2026-01-06 17:04:03 +0530

    Media Fixed

--- src/app/media/page.jsx
+++ src/app/media/page.jsx
@@ -1,44 +1,45 @@
 "use client";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function MediaMarquee() {
   const medias = [
     {
       name: "New York Weekly",
-      link: "https://i0.wp.com/douglasrobbinsauthor.com/storage/2024/01/NY-Weekly.jpg?resize=1024%2C683&ssl=1"
+      link: "/media-images/NY-Weekly.jpeg",
     },
     {
       name: "US Insider",
-      link: "https://cdn.prod.website-files.com/65e83cfcbcd7f076562b441a/65effeeee19c88e19a4de1ef_US%20Insider.png"
+      link: "/media-images/US-Insider.jpeg",
     },
     {
       name: "Indian Bulletin",
-      link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFJhiay9cZPk2CSgS_ruRNcTmpXX3kd5guw&s"
+      link: "/media-images/Indian-Bulletin.jpeg",
     },
     {
       name: "Daily Hunt",
-      link: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Daily_hunt.png"
+      link: "/media-images/Dailyhunt.jpeg",
     },
     {
       name: "News 24",
-      link: "https://upload.wikimedia.org/wikipedia/commons/6/66/News24_Logo.jpg"
+      link: "/media-images/News24.jpeg",
     },
     {
       name: "News Nation",
-      link: "https://play-lh.googleusercontent.com/l01NEnkNP-IjuNmaXMawC4Cy-ikJ5iIDlcRLCukd0ABEVnltCeIft-0HYiF69HfprvdV=w526-h296-rw"
+      link: "/media-images/News-Nation.jpeg",
     },
     {
       name: "Flipboard",
-      link: "https://cdn.flipboard.com/wp-content/uploads/sites/17/2015/02/red-horizontal-379x262.png"
+      link: "/media-images/Flipboard.jpeg",
     },
     {
       name: "Republic",
-      link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtBIe5NZxqZnWJNJz8ae9UPDpN1rSd7o-6Kw&s"
+      link: "/media-images/Republic.jpeg",
     },
     {
       name: "Business Standard",
-      link: "https://www.speakin.co/edit/wp-content/uploads/2019/05/business-standard-logo-2.png"
-    }
+      link: "/media-images/Business-standard.jpeg",
+    },
   ];
 
   return (
@@ -48,7 +49,6 @@ export default function MediaMarquee() {
           Credibility & Trust
         </h2>
 
-       
         <div className="relative overflow-hidden">
           <motion.div
             className="flex gap-16 w-max"
@@ -57,14 +57,16 @@ export default function MediaMarquee() {
               repeat: Infinity,
               repeatType: "loop",
               duration: 30,
-              ease: "linear"
+              ease: "linear",
             }}
           >
             {[...medias, ...medias].map((media, index) => (
-              <img
+              <Image
                 key={index}
                 src={media.link}
                 alt={media.name}
+                width={150}
+                height={50}
                 className="h-[19vw] md:h-[12vw] sm:h-[14vw] object-contain"
               />
             ))}
--- src/app/personality/page.jsx
+++
@@ -1,9 +0,0 @@
-import WhereKidsBuildSection from "../components/WhereKidsBuildSection"
-import {categoryImages} from "../data/categoryImages"
-export default function(){
-    return(
-        <>
-        <WhereKidsBuildSection imageData={categoryImages.personalityEnrichment}/>
-        </>
-    )
-}
